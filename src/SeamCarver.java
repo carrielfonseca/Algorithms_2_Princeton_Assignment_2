0abb93c16c31c716e1514ca9a5a9d5dcd97b5c78
Fabio Carriel Fonseca <ffonseca@modulusdata.com>
2017-12-18 22:21:02 -0500

    main method loads Picture from a File path

--- src/SeamCarver.java
+++ src/SeamCarver.java
@@ -2,10 +2,13 @@ import edu.princeton.cs.algs4.Picture;
 
 public class SeamCarver {
 	
-	private Picture picture;
+	//private Picture picture;
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		
+		Picture picture = new Picture("C:/Users/ffonseca/Desktop/Fabio/HJoceanSmall.png");
+		picture.show();
 		
 		
 

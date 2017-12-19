f2d2c8d8216c4333d1cc7d1a24b52c3d0e4c9efc
Fabio Carriel Fonseca <ffonseca@modulusdata.com>
2017-12-18 23:08:08 -0500

    html assignment specification

--- src/SeamCarver.java
+++ src/SeamCarver.java
@@ -2,13 +2,21 @@ import edu.princeton.cs.algs4.Picture;
 
 public class SeamCarver {
 	
-	//private Picture picture;
+	private Picture picture;
+	
+	public SeamCarver(Picture picture) {
+		this.picture = picture;
+	}
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+	public static void main(String[] args) {	
+		
 		
 		Picture picture = new Picture("C:/Users/ffonseca/Desktop/Fabio/HJoceanSmall.png");
+		SeamCarver seamCarver = new SeamCarver(picture);
+		
 		picture.show();
+		System.out.println(picture.width());
+		System.out.println(picture.height());
 		
 		
 

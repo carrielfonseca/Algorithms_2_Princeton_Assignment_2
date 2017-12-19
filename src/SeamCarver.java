9000bf17cc7c61e279195712da9ffffb2d837f6a
Fabio Carriel Fonseca <ffonseca@modulusdata.com>
2017-12-18 22:06:50 -0500

    changes name of class to SeamCarver

---
+++ src/SeamCarver.java
@@ -0,0 +1,14 @@
+import edu.princeton.cs.algs4.Picture;
+
+public class SeamCarver {
+	
+	private Picture picture;
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		
+
+	}
+
+}

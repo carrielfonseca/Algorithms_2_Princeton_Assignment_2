3134c14781ef93464b265758fb78cfb66bfb1ac1
Fabio Carriel Fonseca <ffonseca@modulusdata.com>
2017-12-21 22:25:47 -0500

    fills out a matrix of Pixels

--- src/SeamCarver.java
+++ src/SeamCarver.java
@@ -1,14 +1,23 @@
+import java.awt.Color;
+
 import edu.princeton.cs.algs4.Picture;
 
 public class SeamCarver {
 	
 	private Picture picture;
-	private int widht, height;	
+	private int widht, height;
+	private Color[][] matrixOfPixels; //Color has 3 coordinates: red,, blue and green  (RBG)
+	private int[][] energy; //the energy represents a distance	
 	
 	public SeamCarver(Picture picture) {
 		this.picture = picture;
 		widht = picture.width();
 		height =  picture.height();
+		for (int i = 0; i < widht ; i++) {
+			for (int j = 0; j < height; j++) {
+				matrixOfPixels[i][j] = picture.get(j, i);	
+			}			
+		}
 	}
 	
 	public int widht() {
@@ -18,6 +27,12 @@ public class SeamCarver {
 	public int height() {
 		return height;
 	}
+	
+	
+//	private int squareOfTheXGradient(int row, int col) {
+//		int deltaRedSquared = matrixOfPixels[row][col-1]
+//		return 
+//	}
 
 	public static void main(String[] args) {	
 		

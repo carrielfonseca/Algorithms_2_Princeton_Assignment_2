d5f36a503a86780b4cbe95e8bbd1168e71cc2b8d
Fabio Carriel Fonseca <ffonseca@modulusdata.com>
2017-12-26 22:22:00 -0500

    calculateEnergyMatrix method takes care of corner cases properly

--- src/SeamCarver.java
+++ src/SeamCarver.java
@@ -226,7 +226,7 @@ public class SeamCarver {
 	private void caculateEnergyMatrix() {
 		for (int i = 0; i < height ; i++) {			
 			for (int j = 0; j < width; j++) {
-				if (i == 0 || j == 0 ) {
+				if (i == 0 || i == (height-1) || j == 0 || j == (width-1)) {
 					energy[i][j] = 1000;
 				}
 				else {

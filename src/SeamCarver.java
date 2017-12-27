7cfae20a76286a078e52f6c0c2efa25553d12374
Fabio Carriel Fonseca <ffonseca@modulusdata.com>
2017-12-26 23:30:54 -0500

    energy method thwors Exception

--- src/SeamCarver.java
+++ src/SeamCarver.java
@@ -61,6 +61,7 @@ public class SeamCarver {
 	}
 	
 	public  double energy(int x, int y)   {            // energy of pixel at column x and row y
+		if (x < 0 || x > (width-1) || y < 0 || y > (height-1)) throw new java.lang.IllegalArgumentException("index out of bound");
 		return energy[y][x];
 	}
 	

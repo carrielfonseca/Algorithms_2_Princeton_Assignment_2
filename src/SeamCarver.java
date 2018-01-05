e7bde7b8604d8c4ad1a9bcab36ee73d8e956a1bc
Fabio Carriel Fonseca <ffonseca@modulusdata.com>
2018-01-04 22:27:46 -0500

    correction of the energy matrix calculation in the borders

--- src/SeamCarver.java
+++ src/SeamCarver.java
@@ -45,7 +45,12 @@ public class SeamCarver {
 	
 	public  double energy(int x, int y)   {            // energy of pixel at column x and row y
 		if (x < 0 || x > (width-1) || y < 0 || y > (height-1)) throw new java.lang.IllegalArgumentException("index out of bound");
-		return Math.sqrt(squareOfTheXGradient(y, x) + squareOfTheYGradient(y, x));
+		if (y == 0 || y == (height-1) || x == 0 || x == (width-1)) {
+			return 1000;
+		} else {
+			return Math.sqrt(squareOfTheXGradient(y, x) + squareOfTheYGradient(y, x));
+		}
+		
 	}
 	
 	public   int[] findVerticalSeam()  {               // sequence of indices for vertical seam
